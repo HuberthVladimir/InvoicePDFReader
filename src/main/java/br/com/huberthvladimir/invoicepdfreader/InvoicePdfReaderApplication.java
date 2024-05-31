3fa3603353398877d1bf0d7ba4cc2d7dea796afe
HuberthVladimir <vladimirhuberth@hotmail.com>
2024-05-30 21:31:56 -0300

    first commmit

---
+++ src/main/java/br/com/huberthvladimir/invoicepdfreader/InvoicePdfReaderApplication.java
@@ -0,0 +1,13 @@
+package br.com.huberthvladimir.invoicepdfreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicePdfReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoicePdfReaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/huberthvladimir/invoicepdfreader/InvoicePdfReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.huberthvladimir.invoicepdfreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoicePdfReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
